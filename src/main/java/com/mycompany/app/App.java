7c9ee795f70af9d5e4b28916ccdfa453f73bf04c
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-10-29 23:14:35 +0530

    changed message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Welcome to AIT";
+    private final String message = "Welcome to My Demonstration";
 
     public App() {}
 
