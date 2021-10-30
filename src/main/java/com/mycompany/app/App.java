56bc047f6c2c4c4ef572a139197c6ea8e9119b38
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-10-30 15:54:09 +0530

    updated message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,12 +4,7 @@ package com.mycompany.app;
  */
 public class App
 {
-
-<<<<<<< HEAD
-    private final String message = "Naya Version Aa Gya :D";
-=======
-    private final String message = "Welcome to This Test";
->>>>>>> 1e00a3c9d6b661c1d5ea7a7d05edbbf1c9635207
+    private final String message = "Hello World!";
 
     public App() {}
 
